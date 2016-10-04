5ee460a0c67e412322d72f1c8a553074c4b46d9e
Maciej Zawiasa <zawiasam@gmail.com>
2016-10-04 12:24:41 +0200

    handle firebase reject query

--- server/commons/auth-firebase.js
+++ server/commons/auth-firebase.js
@@ -2,15 +2,17 @@ import firebase from 'firebase';
 import _ from 'lodash';
 
 export default class AuthFirebase {
-  static getUser(email) {
-    return new Promise((resolve, reject) => {
-      firebase.database().ref("users")
-        .orderByChild('email')
-        .equalTo(email)
-        .once('value', function (snap) {
-          const userObj = snap.val();
-          resolve(userObj);
+    static getUser(email) {
+        return new Promise((resolve, reject) => {
+            firebase.database().ref("users")
+                .orderByChild('email')
+                .equalTo(email)
+                .once('value', (snap) => {
+                    const userObj = snap.val();
+                    resolve(userObj);
+                }, (err) => {
+                    reject(err);
+                });
         });
-    });
-  }
+    }
 }
