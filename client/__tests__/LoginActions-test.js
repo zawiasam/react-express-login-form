33fea6cdc6de5b8ad9c2be511c00779c7e405cbf
Maciej Zawiasa <zawiasam@gmail.com>
2016-10-14 13:35:26 +0200

    should execute login request from LoginStore

--- client/__tests__/LoginActions-test.js
+++ client/__tests__/LoginActions-test.js
@@ -1,11 +1,15 @@
-jest.dontMock('../dev/scripts/app/Login/LoginActions.js');
 import LoginActions from '../dev/scripts/app/Login/LoginActions.js'
+import LoginStore from '../dev/scripts/app/Login/LoginStore.js'
 
 describe("the LoginActions", function loginActionsDescribe() {
-    describe("dispatched paramas", function dispacedParams(){
-        it("should have action type", function itShouldHaveActionType(){
+    describe("action doLoginRequest", function dispacedParams(){
+        it("should execute login request from LoginStore", function itShouldHaveActionType(){
+            let loginRequestMock = jest.fn();
+            LoginStore.loginRequest = loginRequestMock;
+
             LoginActions.doLoginRequest({
-                email: 'mail', password: 'somePassword'})
+                email: 'mail', password: 'somePassword'});
+            expect(loginRequestMock.mock.calls.length).toBe(1);
         })
     })
 })
