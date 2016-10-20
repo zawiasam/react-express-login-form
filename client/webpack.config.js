583ef145dbee3ea7f320c986dd6daf550b546231
Maciej Zawiasa <zawiasam@gmail.com>
2016-10-20 17:42:22 +0200

    code reorganization

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
+'use strict';
+
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var uglifyJsPlugin = require('webpack-uglify-js-plugin');
+var path = require('path');
 
 module.exports = {
   entry: './dev/scripts/app/index.js',
@@ -6,7 +10,7 @@ module.exports = {
   devServer: {
     hot: true
   },
-  
+
   output: {
     filename: 'app/scripts/login/main.js',
     publicPath: ''
@@ -14,19 +18,43 @@ module.exports = {
 
   plugins: [
     new CopyWebpackPlugin([
-      { context: 'dev', from: '*.html', to: 'app/' },
-      { context: 'dev', from: 'css', to: 'app/css/' },
-      { context: 'dev', from: 'libs', to: 'app/libs/' },
-      { context: 'dev', from: 'images', to: 'app/images/' },
-    ])
+      {
+        context: 'dev',
+        from: '*.html',
+        to: 'app/'
+      },
+      {
+        context: 'dev',
+        from: 'css',
+        to: 'app/css/'
+      },
+      {
+        context: 'dev',
+        from: 'libs',
+        to: 'app/libs/'
+      },
+      {
+        context: 'dev',
+        from: 'images',
+        to: 'app/images/'
+      },
+    ]),
+    new uglifyJsPlugin({
+        cacheFolder: path.resolve(__dirname, 'public/cached_uglify/'),
+    })
   ],
 
   module: {
     loaders: [
-      { test: /\.js.{0,1}$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      {
+                    cacheable: true,
+        test: /\.js.{0,1}$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      }
     ]
   },
-  
+
   externals: {
     fs: '{}',
     tls: '{}',
