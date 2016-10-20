0f944821f0d91283192c6c8b5e5f5bbe7b5e83cb
Maciej Zawiasa <zawiasam@gmail.com>
2016-10-20 17:12:27 +0200

    fixed message style

--- client/dev/scripts/modules/Forms/Card.react.js
+++ client/dev/scripts/modules/Forms/Card.react.js
@@ -10,22 +10,28 @@ export default class Card extends React.Component {
     return (
       <div className="demo-card-square mdl-card mdl-shadow--2dp">
         <div className="mdl-card__title">
-          <h2 className="mdl-card__title-text">{message.messageTitle}</h2>
+          <h2 className="mdl-card__title-text">{ message.messageTitle }</h2>
         </div>
         <div className="mdl-card__supporting-text mdl-card--expand">
-          {message.messageBody}
+          { message.messageBody }
         </div>
-        <div className="mdl-card__actions mdl-card--border">
-          <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-            wyświetl
-          </a>
-          <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-            archiwizuj
-          </a>
+        <div className="mdl-card__actions mdl-card--border mdl-grid">
+          <div className="mdl-cell mdl-cell--2-col">
+            <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        wyświetl
+                      </a>
+          </div>
+          <div className="mdl-cell mdl-cell--2-col">
+            <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        archiwizuj
+                      </a>
+          </div>
           <div className="mdl-layout-spacer"></div>
-          <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-            odpowiedz
-          </a>
+          <div className="mdl-cell mdl-cell--2-col">
+            <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        odpowiedz
+                      </a>
+          </div>
         </div>
       </div>
     )
