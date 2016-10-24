9475a26c26ec53954957b2dfec16304dfcad2a42
Maciej Zawiasa <zawiasam@gmail.com>
2016-10-24 05:48:26 +0200

    add new message btn

--- client/dev/scripts/modules/Forms/Dashboard/MainContent.react.js
+++ client/dev/scripts/modules/Forms/Dashboard/MainContent.react.js
@@ -8,13 +8,16 @@ export default class DashboardSideMenu extends React.Component {
 	render() {
 		const message = this.props.message;
 		return (
-		<main className="mdl-layout__content mdl-color--grey-100">
-			<div className="mdl-grid demo-content">
-				<div className="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
-          {this.props.children}
-				</div>
-			</div>
-		</main>
+			<main className="mdl-layout__content mdl-color--grey-100">
+     <div className="mdl-grid demo-content">
+       <div className="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+         <button id="btn-new_message" className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+           <i className="material-icons">add</i>
+         </button>
+         { this.props.children }
+       </div>
+     </div>
+   </main>
 		)
 	}
 }
