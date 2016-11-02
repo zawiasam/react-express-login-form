f27ff25c7d7bc4534acc6bf0b71684b5f2eff5ad
Maciej Zawiasa <zawiasam@gmail.com>
2016-11-02 11:34:06 +0100

    LoginActions test

--- client/__tests__/LoginActions-test.js
+++ client/__tests__/LoginActions-test.js
@@ -2,14 +2,29 @@ import LoginActions from '../dev/scripts/app/Login/LoginActions.js'
 import LoginStore from '../dev/scripts/app/Login/LoginStore.js'
 
 describe("the LoginActions", function loginActionsDescribe() {
-    describe("action doLoginRequest", function dispacedParams(){
-        it("should execute login request from LoginStore", function itShouldHaveActionType(){
-            let loginRequestMock = jest.fn();
+    describe("action doLoginRequest", function dispacedParams() {
+        let loginRequestMock;
+        let credentials;
+        beforeEach(function setupMock() {
+            loginRequestMock = jest.fn();
             LoginStore.loginRequest = loginRequestMock;
 
-            LoginActions.doLoginRequest({
-                email: 'mail', password: 'somePassword'});
-            expect(loginRequestMock.mock.calls.length).toBe(1);
+            credentials = {
+                email: 'mail',
+                password: 'somePassword'
+            };
+        })
+
+        it("should execute login request from LoginStore", function itShouldHaveActionType() {
+            LoginActions.doLoginRequest(credentials);
+
+            expect(loginRequestMock).toHaveBeenCalledTimes(1);
+        })
+
+        it("should execute login request with proper credentials", function itShouldHaveActionType() {
+            LoginActions.doLoginRequest(credentials);
+
+            expect(loginRequestMock).toBeCalledWith(credentials);
         })
     })
 })
