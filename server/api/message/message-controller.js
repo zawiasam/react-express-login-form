04da6616c6f187c0ae0488694282742c9e7afdf1
Maciej Zawiasa <zawiasam@gmail.com>
2016-12-11 12:49:04 +0100

    before sending a message validates if user exists

--- server/api/message/message-controller.js
+++ server/api/message/message-controller.js
@@ -1,6 +1,39 @@
 import dbFirebase from '../../commons/db-firebase.js';
+import userFirebase from '../../commons/user-firebase.js';
+import Logger from '../../commons/logger.js';
 import _ from 'lodash';
 import moment from 'moment';
+
+class MessagesCtrlHelpers {
+
+  static getExistingAndNotExistingUsers(userIdArray) {
+    return new Promise((res, rej) => {
+      let existingUsers = [];
+      let nonExistingUsers = [];
+      let fillfull = 0;
+      userIdArray.forEach((user) => {
+        userFirebase.doesExist(user).then((does) => {
+          if (does) {
+            existingUsers.push(user);
+          } else {
+            nonExistingUsers.push(user);
+          }
+          fillfull++;
+          if (fillfull === userIdArray.length) {
+            res({
+              existingUsers: existingUsers,
+              nonExistingUsers: nonExistingUsers,
+            })
+          }
+        }, (err) => {
+          rej({
+            fillfull: fillfull
+          });
+        })
+      });
+    })
+  }
+}
 
 export default class MessagesCtrl {
   static getAllMessages(req, res) {
@@ -13,21 +46,36 @@ export default class MessagesCtrl {
 
   static sendMessage(req, res) {
     let message = _.pick(req.body, ["title", "body", "receivers"]);
-    let transmitTo = message.receivers;
 
-    message.createdAt = moment().format();
-    dbFirebase.pushItem("messageList", message).then((r) => {
-      let messages = {};
-      transmitTo.forEach((receiver) => {
-        messages[`userData/${receiver}/messages/${r.key}`] = {status: "unread"}
-      }, this);
-      dbFirebase.pushObject(messages).then(() => {
-        res.status(200).send();
-      }, (err) => {
-        res.status(500).json(err);
+    MessagesCtrlHelpers.getExistingAndNotExistingUsers(message.receivers).then((val) => {
+      return new Promise((res) => {
+        if (val.nonExistingUsers.length) {
+          Logger.bizWarning(`some receivers are not accessible ${JSON.stringify(val.nonExistingUsers)}`)
+        }
+        res(val.existingUsers || []);
       })
-    }, (e) => {
-      res.status(500).json(err);
-    });
+    }, (err) => {
+      Logger.bizError(`could not validate all users. Fillfulled ${err.fillfull}`)
+      return new Promise((res)=> {
+        res([]);
+      })
+    }).then((users) => {
+      message.createdAt = moment().format();
+      dbFirebase.pushItem("messageList", message).then((messageRef) => {
+        let messages = {};
+        users.forEach((receiver) => {
+          messages[`userData/${receiver}/messages/${messageRef.key}`] = {
+            status: "unread"
+          }
+        }, this);
+        dbFirebase.updateObject(messages).then(() => {
+          res.status(200).send();
+        }, (err) => {
+          res.status(500).json(err);
+        })
+      }, (e) => {
+        res.status(500).json(err);
+      });
+    })
   }
 }
--- server/commons/logger.js
+++ server/commons/logger.js
@@ -1,32 +1,105 @@
 import winston from 'winston';
 
+/**
+ * winston logger class
+ * @typedef WinstonLogger
+ * @property {function} error - error logger function
+ * @property {function} warn - warn logger function
+ * @property {function} info - info logger function
+ */
+
+/**
+ * Logger types
+ * @typedef {('info'|'warn'|'error')} LogType - Allowed Logger types
+ */
+
+/**
+ * Enum of logger levels
+ * @readonly 
+ * @enum {string}
+ */
+let errorLevel = {
+  error: 'error',
+  warn: 'warn',
+  info: 'info',
+}
+
 let dbLogger = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)({
-      level: 'error'
+      level: 'info'
     }),
     new (winston.transports.File)({
-      filename: "firebase-error.log",
-      level: "info"
+      filename: "db-logger.log",
+      level: "warn"
     })
   ]
 });
 
-class Logger {
-  static _log (logger, level, message) {
+let bizLogger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      level: 'info'
+    }),
+    new (winston.transports.File)({
+      filename: "biz-logger.log",
+      level: "warn"
+    })
+  ]
+});
+
+class PrivertLogger {
+  /**
+   * log message using given logger, at given level, with given message
+   * @param {WinstonLogger} logger instance of logger
+   * @param {LogType} level one of kwnon levels [info|warn|error]
+   * @param {string} message description
+   */
+  static log(logger, level, message) {
     logger[level](message);
   }
+}
+
+export default class Logger {
   /**
    * log message on given level
    * @param {string} level level of log entry
    * @param {string} message message to log
    */
   static dbLog(level, message) {
-    _log(dbLogger, level, message);
+    PrivertLogger.log(dbLogger, level, message);
   }
+
+  /**
+   * log db error
+   * @param {string} message error message
+   */
   static dbLogError(message) {
-    dbLog('error', message);
+    this.dbLog('error', message);
+  }
+
+  /**
+   * log biz event with given level
+   * @param {LogType} level level of log message
+   * @param {string} message description
+   */
+  static bizLog(level, message) {
+    PrivertLogger.log(bizLogger, level, message)
+  }
+
+  /**
+   * log biz error
+   * @param {string} message description
+   */
+  static bizError(message) {
+    this.bizLog("error", message);
+  }
+
+  /**
+   * log biz warning
+   * @param {string} message description
+   */
+  static bizWarning(message) {
+    this.bizLog("warn", message);
   }
 }
-
-export default Logger
